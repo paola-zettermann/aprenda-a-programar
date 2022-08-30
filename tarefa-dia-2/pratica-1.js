6fc1e38c13b9e4e18227f0a5a572d0ad14e000f7
Paola <paola.zettermann@gmail.com>
2022-08-30 20:29:21 -0300

    mensagem de commit

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,5 +1,11 @@
 let nome = "Paola";
 let email = "paola.zettermann@gmail.com";
-let profissão = "estudante";
+let profissao = "estudante";
 let idade = 18;
 let lugar = "Estados Unidos";
+
+console.log(nome);
+console.log(email);
+console.log(profissao);
+console.log(idade);
+console.log(lugar);
