198822f5df2223a537cc4253d9ba6590d63faa13
Paola <paola.zettermann@gmail.com>
2022-08-30 23:24:58 -0300

    mensagem de commit

---
+++ tarefa-dia-3/pratica-2.js
@@ -0,0 +1,10 @@
+let multiplicador = 20
+let n = 10
+let i = 1
+
+while (i <= n){
+    let resultado = multiplicador * i
+    let texto = multiplicador + " X " + i + " = " + resultado
+    console.log(texto)
+    i++
+}
